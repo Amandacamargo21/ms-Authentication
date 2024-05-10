e07a7aa57a6996e117e93f181bcc0763292d9ec6
Amanda Camargo <98326750+Amandacamargo21@users.noreply.github.com>
2024-05-10 16:54:55 -0300

    finalizando api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'bar'})
-});
+//configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
 
+//configuração de rotas
+app.use(usersRoute);
+app.use(statusRoute);
+
+//Inicialização do servidor
 app.listen(3000, () => {
     console.log('executando ihull');
 })
